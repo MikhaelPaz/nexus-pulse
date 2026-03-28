c287d1de330188251781595b0b908630d20066f8
pc_HT <ht12bilmontanha@gmail.com>
2026-03-28 10:15:40 -0300

    painel lateral com info automaticas

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function mostrarDados(nome, info) {
+function mostrarDados(nome, info, followers, followers_number, viewers) {
     if (painel.style.display === 'none') {
         const painel = document.getElementById("painel");
         const nomeEl = document.getElementById("nome");
         const infoEl = document.getElementById("info");
+        const followersEl = document.getElementById("followers");
+        const followers_numberEl = document.getElementById("followers_number");
+        const viewersEl = document.getElementById("viewers");
+
 
         nomeEl.textContent = nome;
         infoEl.textContent = info;
+        followersEl.textContent = followers;
+        followers_numberEl.textContent = followers_number;
+        viewersEl.textContent = viewers;
+
+
 
         painel.style.display = "block";
     }
