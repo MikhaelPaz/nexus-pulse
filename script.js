5c5ec2e69aa31062ebc9dd3affa2adaeafb0a2bb
William <williamwagner848@gmail.com>
2026-04-14 20:17:43 -0300

    index/fix

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-function mostrarDados(nome, info, followers, followers_number, viewers) {
-    if (painel.style.display === 'none') {
-        const painel = document.getElementById("painel");
-        const nomeEl = document.getElementById("nome");
-        const infoEl = document.getElementById("info");
-        const followersEl = document.getElementById("followers");
-        const followers_numberEl = document.getElementById("followers_number");
-        const viewersEl = document.getElementById("viewers");
+function mostrarDadosStreamers(nome, info, followers, followers_number, viewers) {
+    const painel = document.getElementById("painel");
+    const nomeEl = document.getElementById("nome");
+    const infoEl = document.getElementById("info");
+    const followersLabelEl = document.getElementById("followers-label");
+    const followersNumberEl = document.getElementById("followers_number");
+    const viewersEl = document.getElementById("viewers");
+    const aproveitamentoEl = document.getElementById("aproveitamento");
 
+    nomeEl.textContent = nome;
+    infoEl.textContent = info;
+    followersLabelEl.textContent = followers;
+    followersNumberEl.textContent = followers_number;
+    viewersEl.textContent = viewers;
+    aproveitamentoEl.textContent = "-";
 
-        nomeEl.textContent = nome;
-        infoEl.textContent = info;
-        followersEl.textContent = followers;
-        followers_numberEl.textContent = followers_number;
-        viewersEl.textContent = viewers;
+    painel.style.display = "block";
+}
 
+function mostrarDadosJogadores(nome, info, valorAproveitamento) {
+    const painel = document.getElementById("painel");
+    const nomeEl = document.getElementById("nome");
+    const infoEl = document.getElementById("info");
+    const followersLabelEl = document.getElementById("followers-label");
+    const followersNumberEl = document.getElementById("followers_number");
+    const viewersEl = document.getElementById("viewers");
+    const aproveitamentoEl = document.getElementById("aproveitamento");
 
+    nomeEl.textContent = nome;
+    infoEl.textContent = info;
+    followersLabelEl.textContent = "Followers";
+    followersNumberEl.textContent = "-";
+    viewersEl.textContent = "-";
+    aproveitamentoEl.textContent = valorAproveitamento;
 
-        painel.style.display = "block";
-    }
-    else{
-        painel.style.display = 'none';
-    }
+    painel.style.display = "block";
 }
